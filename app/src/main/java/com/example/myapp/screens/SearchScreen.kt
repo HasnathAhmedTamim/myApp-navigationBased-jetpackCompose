d9150d3927e339b6282c125e714d3fbcee984e05
HasnathAhmedTamim <hasnath.tamim333@gmail.com>
2026-01-22 11:43:24 +0600

    Create Search Screen

---
+++ app/src/main/java/com/example/myapp/screens/SearchScreen.kt
@@ -0,0 +1,15 @@
+package com.example.myapp.screens
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SearchScreen(
+    onBack: () -> Unit,
+    onHomeClick: () -> Unit,
+    onSearchClick: () -> Unit,
+    onAlbumClick: () -> Unit
+) {
+    //    Todo
+}
