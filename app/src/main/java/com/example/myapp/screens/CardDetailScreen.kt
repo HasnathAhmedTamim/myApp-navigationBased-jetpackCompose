a1a4090530081a40b161defe105c330ae8723b42
HasnathAhmedTamim <hasnath.tamim333@gmail.com>
2026-01-22 14:26:48 +0600

    Card Detail Screen Implemented

--- app/src/main/java/com/example/myapp/screens/CardDetailScreen.kt
+++ app/src/main/java/com/example/myapp/screens/CardDetailScreen.kt
@@ -1,7 +1,22 @@
 package com.example.myapp.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -11,4 +26,28 @@ fun CardDetailScreen(
     onBack: () -> Unit
 ){
     //    Todo
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(cardTitle) },
+                navigationIcon = {
+                    IconButton(onClick = onBack) {
+                        Icon(Icons.AutoMirrored.Filled.ArrowBack, "Back")
+                    }
+                }
+            )
+        }
+    ) {padding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+                .padding(24.dp)
+        ) {
+            Text("Card ID: $cardId", style = MaterialTheme.typography.headlineMedium)
+            Spacer(modifier = Modifier.height(16.dp))
+            Text("This is the detail page for $cardTitle")
+        }
+
+    }
 }
