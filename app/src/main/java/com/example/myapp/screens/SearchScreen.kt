c76bbd8edcb5761ace92cf2b15dc37bf3b22cc9f
HasnathAhmedTamim <hasnath.tamim333@gmail.com>
2026-01-22 12:30:22 +0600

    Search Screen Implemented

--- app/src/main/java/com/example/myapp/screens/SearchScreen.kt
+++ app/src/main/java/com/example/myapp/screens/SearchScreen.kt
@@ -1,7 +1,32 @@
 package com.example.myapp.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.PhotoAlbum
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -12,4 +37,59 @@ fun SearchScreen(
     onAlbumClick: () -> Unit
 ) {
     //    Todo
+    var searchQuery by remember { mutableStateOf("") }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Search") },
+                navigationIcon = {
+                    IconButton(onClick = onBack) {
+                        Icon(Icons.AutoMirrored.Filled.ArrowBack, "Back")
+                    }
+                }
+            )
+        },
+        bottomBar = {
+            NavigationBar {
+                NavigationBarItem(
+                    icon = { Icon(Icons.Default.Home, "Home") },
+                    label = { Text("Home") },
+                    selected = false,
+                    onClick = onHomeClick
+                )
+                NavigationBarItem(
+                    icon = { Icon(Icons.Default.Search, "Search") },
+                    label = { Text("Search") },
+                    selected = true,
+                    onClick = onSearchClick
+                )
+                NavigationBarItem(
+                    icon = { Icon(Icons.Default.PhotoAlbum, "Album") },
+                    label = { Text("Album") },
+                    selected = false,
+                    onClick = onAlbumClick
+                )
+            }
+        }
+
+    ){padding->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+                .padding(16.dp)
+        ){
+            OutlinedTextField(
+                value = searchQuery,
+                onValueChange = { searchQuery = it },
+                modifier = Modifier.fillMaxWidth(),
+                placeholder = { Text("Search...") },
+                leadingIcon = { Icon(Icons.Default.Search, null) }
+            )
+            Spacer(modifier = Modifier.height(24.dp))
+            Text("Search Screen Content")
+        }
+
+    }
 }
