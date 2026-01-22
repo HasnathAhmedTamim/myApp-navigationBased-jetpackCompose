965447435add4df2eab5389c1ed06d82284c6844
HasnathAhmedTamim <hasnath.tamim333@gmail.com>
2026-01-22 12:14:11 +0600

    Profile Screen Implemented

--- app/src/main/java/com/example/myapp/screens/ProfileScreen.kt
+++ app/src/main/java/com/example/myapp/screens/ProfileScreen.kt
@@ -1,7 +1,21 @@
 package com.example.myapp.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.myapp.model.User
 
 
@@ -12,4 +26,32 @@ fun ProfileScreen(
     onBack: () -> Unit
 ){
     //    Todo
+
+    Scaffold(
+        //Display Profile top bar with back button
+        topBar = {
+            TopAppBar(
+                title = { Text("Profile") },
+                navigationIcon = {
+                    IconButton(onClick = onBack) {
+                        Icon(Icons.AutoMirrored.Filled.ArrowBack, "Back")
+                    }
+                }
+            )
+        }
+    ){padding->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+                .padding(24.dp)
+        ){
+            Text("Username: ${user?.username ?: "N/A"}")
+            Spacer(modifier = Modifier.height(16.dp))
+            Text("Email: ${user?.email ?: "N/A"}")
+            Spacer(modifier = Modifier.height(16.dp))
+            Text("Phone: ${user?.phone ?: "N/A"}")
+        }
+
+    }
 }
