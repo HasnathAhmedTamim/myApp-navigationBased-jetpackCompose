b34b34591aa324a22a47c2777bd34674f47c9844
HasnathAhmedTamim <hasnath.tamim333@gmail.com>
2026-01-22 11:43:16 +0600

    Create Profile Screen

---
+++ app/src/main/java/com/example/myapp/screens/ProfileScreen.kt
@@ -0,0 +1,15 @@
+package com.example.myapp.screens
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import com.example.myapp.model.User
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ProfileScreen(
+    user: User?,
+    onBack: () -> Unit
+){
+    //    Todo
+}
