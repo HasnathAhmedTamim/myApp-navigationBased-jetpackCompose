0791a8090140e39eea1d3f907cfc3fb03287aeea
HasnathAhmedTamim <hasnath.tamim333@gmail.com>
2026-01-22 14:06:45 +0600

    Album Item Detail Screen Implemented

--- app/src/main/java/com/example/myapp/screens/AlbumItemDetailScreen.kt
+++ app/src/main/java/com/example/myapp/screens/AlbumItemDetailScreen.kt
@@ -1,8 +1,27 @@
 package com.example.myapp.screens
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import com.example.myapp.model.AlbumItem
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -11,4 +30,46 @@ fun AlbumItemDetailScreen(
     onBack: () -> Unit
 ){
     //    Todo
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Album Detail") },
+                navigationIcon = {
+                    IconButton(onClick = onBack) {
+                        Icon(Icons.AutoMirrored.Filled.ArrowBack, "Back")
+                    }
+                }
+            )
+        }
+    ){paddingValues ->
+        // Display Album Item details here using 'item'
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+                .padding(16.dp)
+        ){
+            if (item == null) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(paddingValues),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text("Item not found")
+                }
+            } else {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(paddingValues)
+                        .padding(24.dp)
+                ) {
+                    Text(item.title, style = MaterialTheme.typography.headlineMedium)
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Text(item.description)
+                }
+            }
+        }
+    }
 }
