610eb36c622bff051fc18ab3b905ff284a74b498
HasnathAhmedTamim <hasnath.tamim333@gmail.com>
2026-01-22 11:42:51 +0600

    Create Album Item Details Screen

---
+++ app/src/main/java/com/example/myapp/screens/AlbumItemDetailScreen.kt
@@ -0,0 +1,14 @@
+package com.example.myapp.screens
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import com.example.myapp.model.AlbumItem
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AlbumItemDetailScreen(
+    item: AlbumItem?,
+    onBack: () -> Unit
+){
+    //    Todo
+}
