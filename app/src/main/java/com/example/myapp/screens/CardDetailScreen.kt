15ba3998f26cac579c24cdb80d93866ccc1d36b6
HasnathAhmedTamim <hasnath.tamim333@gmail.com>
2026-01-22 11:43:03 +0600

    Create Card Details Screen

---
+++ app/src/main/java/com/example/myapp/screens/CardDetailScreen.kt
@@ -0,0 +1,14 @@
+package com.example.myapp.screens
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CardDetailScreen(
+    cardId: String,
+    cardTitle: String,
+    onBack: () -> Unit
+){
+    //    Todo
+}
